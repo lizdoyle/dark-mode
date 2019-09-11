7d27a69a76c1abefb748ec81a5887da0c496f1f3
lizdoyle <lizdoyle1121@gmail.com>
2019-09-11 00:39:35 -0400

    fix to useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -8,7 +8,7 @@ export const useLocalStorage = (key, initialValue) => {
 
     const setValue = value => {
         setStoredValue(value);
-        const value = window.localStorage.setItem(key, JSON.stringify(value));
+        const val = window.localStorage.setItem(key, JSON.stringify(val));
 
     }
     return [storedValue, setValue];
